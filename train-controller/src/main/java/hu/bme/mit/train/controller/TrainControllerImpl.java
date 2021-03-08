f607a3dfcab12fdca4b566b59a86d5b9da5ba24e
kayyer <61737188+kayyer@users.noreply.github.com>
2021-03-08 22:10:19 +0100

    error taken out of the code

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -47,7 +47,7 @@ public class TrainControllerImpl implements TrainController {
 	}
 
 	public void writeSpeed(){
-		System.ut.println("The reference speed is: " +  referenceSpeed);
+		System.out.println("The reference speed is: " +  referenceSpeed);
 		System.out.println("The speed limit is: " + speedLimit);
 	}
 }
